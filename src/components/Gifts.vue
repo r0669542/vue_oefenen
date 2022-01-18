72ea104f8b980c97953b7f43bed4ca9dd46aa001
r0669542 <r0669542@student.thomasmore.be>
2022-01-18 15:04:56 +0100

    commit2

---
+++ src/components/Gifts.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>
+            Gifts
+        </h1>
+        <div v-for="gift in gifts" :key="gift.id">
+            {{ gift.name}} - {{ gift.receiver }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Homepage",
+        data() {
+            return {
+                gifts : []
+            }
+        },
+        mounted() {
+            this.fetchGifts();
+        },
+        methods: {
+            fetchGifts() {
+                const url = "http://localhost:41391/Gift";
+                fetch(url)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((gifts) => {
+                        
+                        this.gifts = gifts;
+                        console.log(gifts);
+                    }
+                );
+            },
+        }
+    }
+</script>
